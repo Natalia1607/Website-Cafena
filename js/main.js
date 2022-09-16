76623c85a9abb3c846ad73a9c7fdb90723b1c5db
Natalia Gilyazova <gilyazovan97@mail.ru>
2022-09-16 14:09:04 +0300

    modified styles and added header-scroll

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ const prev = document.getElementById('btn_prev'),
 
 let index = 0;
 
+/* Slider */
 const activeSlide = n => {
     for(slide of slides) {
         slide.classList.remove('active');
     }
     slides[n].classList.add('active');
-}
+};
 
 const prepareCurrentSlide = int => {
     activeSlide(index);
-}
+};
 
 const nextSlide = () => {
     if(index == slides.length - 1) {
@@ -23,7 +24,7 @@ const nextSlide = () => {
         index++;
         prepareCurrentSlide(index);
     }
-}
+};
 
 const prevSlide = () => {
     if(index == 0) {
@@ -33,7 +34,23 @@ const prevSlide = () => {
         index--;
         prepareCurrentSlide(index);
     }
+};
+
+/*Scroll header */
+function scrollHeader(){
+    const header = document.getElementById('header');
+    const logotip = document.getElementById('logotip');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+        logotip.src = './../img/header-logo-light.png';
+    } 
+    else {
+        header.classList.remove('scroll-header');
+        logotip.src = './../img/header-logo.png';
+    }
 }
 
+window.addEventListener('scroll', scrollHeader);
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
